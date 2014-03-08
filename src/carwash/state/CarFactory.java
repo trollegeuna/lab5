fe39222357ecc990dff2213bf1a5ea3162b694b9
Johannes Sjölund <j.sjolund@gmail.com>
2014-03-08 16:40:42 +0100

    Kanske inte bästa lösningen, men den duger tills vidare tror jag

--- src/carwash/state/CarFactory.java
+++ src/carwash/state/CarFactory.java
@@ -2,13 +2,14 @@ package carwash.state;
 
 public class CarFactory {
 	private int index = -1;
-
-	public CarFactory() {
-
+	CarWashState carWashState;
+	
+	public CarFactory(CarWashState carWashState) {
+		this.carWashState = carWashState;
 	}
 
 	public Car makeCar() {
 		index++;
-		return new Car(index);
+		return new Car(index, carWashState.currentTime);
 	}
 }
