c210ce085a2b11e5ab35bcd4943c83da856947e2
Johannes Sjölund <j.sjolund@gmail.com>
2014-03-08 16:38:45 +0100

    De viktigaste funktionerna

--- src/lab5/simulator/EventQueue.java
+++ src/lab5/simulator/EventQueue.java
@@ -1,5 +1,31 @@
 package lab5.simulator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class EventQueue {
 
+	private List<Event> eventQueue = new ArrayList<Event>();
+
+	public void add(Event event) {
+		eventQueue.add(event);
+		Collections.sort(eventQueue);
+	}
+
+	public int size() {
+		return eventQueue.size();
+	}
+
+	public Event first() {
+		return eventQueue.get(0);
+	}
+
+	public void removeFirst() {
+		eventQueue.remove(0);
+	}
+
+	public void clear() {
+		eventQueue.clear();
+	}
 }
