6cdced4ae25087dea5f6d7abb133ad042eba0c68
Johannes Sjölund <j.sjolund@gmail.com>
2014-03-08 16:38:25 +0100

    Implementerade Comparator

--- src/lab5/simulator/Event.java
+++ src/lab5/simulator/Event.java
@@ -2,7 +2,7 @@ package lab5.simulator;
 
 import carwash.state.CarWashState;
 
-public abstract class Event {
+public abstract class Event implements Comparable<Event>{
 	public String name;
 	public double startTime;
 
@@ -15,4 +15,15 @@ public abstract class Event {
 	}
 
 	public abstract void execute();
+	
+	@Override
+	public int compareTo(Event otherEvent) {
+		if (this.startTime == otherEvent.startTime) {
+			return 0;
+		} else if (this.startTime < otherEvent.startTime) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
 }
