b02c69275386dfc16aad7b2ebc10c20a60a8404b
Johannes Sjölund <j.sjolund@gmail.com>
2014-03-08 16:40:23 +0100

    Fixade några grejer som saknades

--- src/carwash/state/CarWashState.java
+++ src/carwash/state/CarWashState.java
@@ -1,6 +1,5 @@
 package carwash.state;
 
-
 import carwars.events.SimulationStarts;
 import lab5.simulator.EventQueue;
 import lab5.simulator.SimState;
@@ -10,9 +9,9 @@ public class CarWashState extends SimState {
 	final public int totalSlowWashers = 2;
 	public int availableFastWashers;
 	public int availableSlowWashers;
-	public int currentTime = super.currentTime;
 
-	private long seed = 1234;
+	public double stopTime;
+	public double currentTime = super.currentTime;
 
 	public UniformRandomStream fastURS;
 	public UniformRandomStream slowURS;
@@ -21,17 +20,27 @@ public class CarWashState extends SimState {
 	private double[] fastDist = new double[] { 2.8, 4.6 };
 	private double[] slowDist = new double[] { 3.5, 6.7 };
 	private double lambda = 2;
+	private long seed = 1234;
+
 	public double totalQueueTime;
 	public double totalIdleTime;
 	public int totalRejected;
 
 	public FIFO carQueue;
-	public CarFactory carFactory = new CarFactory();
+	public CarFactory carFactory;
 	public EventQueue eventQueue;
-	
-	public CarWashState(EventQueue eventQueue) {
+
+	public CarWashState(EventQueue eventQueue, double stopTime) {
+		this.stopTime = stopTime;
 		this.eventQueue = eventQueue;
 		eventQueue.add(new SimulationStarts(this, eventQueue));
+		carFactory = new CarFactory(this);
+		carQueue = new FIFO();
+		fastURS = new UniformRandomStream(fastDist[0], fastDist[1], seed);
+		slowURS = new UniformRandomStream(slowDist[0], slowDist[1], seed);
+		eRS = new ExponentialRandomStream(lambda, seed);
+		availableFastWashers = totalFastWashers;
+		availableSlowWashers = totalSlowWashers;
 	}
 
 	public boolean isFull() {
@@ -39,12 +48,6 @@ public class CarWashState extends SimState {
 			return true;
 		}
 		return false;
-	}
-
-	public CarWashState() {
-		fastURS = new UniformRandomStream(fastDist[0], fastDist[1], seed);
-		slowURS = new UniformRandomStream(slowDist[0], slowDist[1], seed);
-		eRS = new ExponentialRandomStream(lambda, seed);
 	}
 
 }
